4cf21c32b8243b6317f90268ce5ab496e2175851
LHJ1355 <lhj56411355@gmail.com>
2021-08-01 19:24:40 +0900

    first commit

---
+++ src/main/java/spring/study/security/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package spring.study.security.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .mvcMatchers("/", "/info", "/login").permitAll()
+                .mvcMatchers("/admin").hasRole("ADMIN")
+                .anyRequest().authenticated();
+
+//        스프링 시큐리티가 제공하는 로그인 페이지 사용 여부
+//        사용하지 않으면 개발자가 정의한 /login 컨트롤러로 로그인 페이지 요청
+        http.formLogin();
+        http.httpBasic();
+    }
+}
